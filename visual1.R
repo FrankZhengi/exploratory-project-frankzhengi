c33ca3644757b61390e2c9ebfe223eecf0ac4472
UW <jyb51@uw.edu>
2022-05-22 23:35:34 -0700

    index graph

--- visual1.R
+++ visual1.R
@@ -10,16 +10,21 @@ library(ggplot2)
 
 gasData <- read.csv('data/usReport.csv', skip = 6)
 
-ggplot(data=gasData[25:47,], aes(x=Year, y=Jan)) +
-  geom_bar(stat="identity", width=0.5)
 
 
+# Price of gasoline in the month of January from the years 2000 - 2022 ( 20th Century)
+Jan_gasincrease <- ggplot(data=gasData[25:47,], aes(x=Year, y=Jan)) +
+  geom_bar(stat="identity",color = "white",fill="black" ,width = 0.3)
+
+
+
+#color change
 ggplot(data=gasData, aes(x=Year, y=Jan)) +
-  geom_bar(stat="identity", color="white", fill="black")
+   geom_bar(stat="identity", color="white", fill="black")
 
 
 p<-ggplot(data=gasData, aes(x=Year, y=Jan)) +
-  geom_bar(stat="identity", fill="red")+
+  geom_bar(stat="identity", fill="white")+
   theme_minimal()
   
 
