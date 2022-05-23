a93488d436fd10b1a56aed9277e677c627af8724
Frank <frankzheng20@outlook.com>
2022-05-22 23:58:19 -0700

    finished

--- visual2.R
+++ visual2.R
@@ -1,11 +1,11 @@
 library(ggplot2)
 
-us_city_average <- read.csv("SeriesReport-20220523022357_8c154c.csv", skip = 8)
+us_city_average <- read.csv("data/usReport.csv", skip = 6)
 
 # average pricing for January starting from 1976 to 2022
 
-ggplot(data = us_city_average) +
-  geom_point(mapping = aes(x = X, y = X.1)) + 
+us_city_plot <- ggplot(data = us_city_average) +
+  geom_point(mapping = aes(x = Year, y = Jan)) + 
   labs(x = "Year", y = "Price")
 
 
