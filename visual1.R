7781e70c03ce1db014bf7907bebfb1e55c0942df
UW <jyb51@uw.edu>
2022-05-22 23:03:33 -0700

    stupid bar graph

--- visual1.R
+++ visual1.R
@@ -2,78 +2,25 @@ library(tidyr)
 library(dplyr)
 library(stringr)
 library(ggplot2)
-library(ggrepel)
-
-
-
-
-
-gasData <- read.csv('data/gasprice91_16.csv', skip = 4)
-
-names(gasData) = gsub(pattern = "X", replacement = "", x = names(gasData))
 
 
 
 
 
 
+gasData <- read.csv('data/usReport.csv', skip = 6)
+
+ggplot(data=gasData[25:47,], aes(x=Year, y=Jan)) +
+  geom_bar(stat="identity", width=0.5)
 
 
+ggplot(data=gasData, aes(x=Year, y=Jan)) +
+  geom_bar(stat="identity", color="white", fill="black")
 
 
-# years <- gasData%>% 
-#   select(36:66)
-
-df_column1 <- gasData %>%
-  pivot_longer(
-    cols = 36:66,
-    names_to = "names",
-    values_to = "values"
-  ) %>%
-  filter(grepl('Number', names))
-
-
-df_duration <- gasData %>%
-  pivot_longer(
-    cols = 36:66,
-    names_to = "names",
-    values_to = "values"
-  )%>%
-  filter(grepl('Duration', names))
-
-plotCol1 <- ggplot(df_column1, aes(x = factor(years), y = values)) + 
-  geom_bar(stat = "identity") + 
-  xlab("Year") +
-  ylab("Price")+
-  #facet_wrap(~Group)+
-  theme_bw()
-
-plot_Year <- ggplot(df_duration, aes(x=factor(years), y = values)) + 
-  geom_bar(stat = "identity")+
-  xlab("Year") +
-  ylab("Price") +
-  #facet_wrap(~Group) + 
-  theme_bw()
-
-
-plot_grid(plotCol1, plot_Year, labels = "AUTO")
-  
-  
-  
-  
-  
-  
-
-
-
-
-
-# Don't know if this is necessary, someone double check with me here 
-#gasData[,5:66] <- sapply(gasData[,5:66], as.numeric)
-
-
-
-  
+p<-ggplot(data=gasData, aes(x=Year, y=Jan)) +
+  geom_bar(stat="identity", fill="red")+
+  theme_minimal()
   
 
   
